9e735beae99197358a85411aa021f4b76866d73e
williamzhou8818 <william.zhou8818@gmail.com>
2025-10-21 21:36:52 +0900

    before deploy to ec2

---
+++ .platform/hooks/predeploy/01_set_exec.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# 给 Go 可执行文件加执行权限
+chmod +x /var/app/staging/main
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"net/http"
+    "os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Transaction struct {
+    ID       int     `json:"id"`
+    Type     string  `json:"type"`     // 收入/支出
+    Category string  `json:"category"` // 分类
+    Amount   float64 `json:"amount"`   // 金额
+}
+
+func main() {
+        // 创建默认 Gin 引擎
+    r := gin.Default()
+
+    
+	// 跨域 (CORS)
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
+
+	// 模拟数据
+	transactions := []Transaction{
+		{ID: 1, Type: "income", Category: "Salary", Amount: 5000},
+		{ID: 2, Type: "expense", Category: "Food", Amount: 120},
+		{ID: 3, Type: "expense", Category: "Transport", Amount: 80},
+	}
+
+	// GET /api/transactions
+	r.GET("/api/transactions", func(c *gin.Context) {
+		c.JSON(http.StatusOK, transactions)
+	})
+
+	// POST /api/transactions
+	r.POST("/api/transactions", func(c *gin.Context) {
+		var t Transaction
+		if err := c.BindJSON(&t); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		t.ID = len(transactions) + 1
+		transactions = append(transactions, t)
+		c.JSON(http.StatusCreated, t)
+	})
+
+	// 健康检查（AWS 用）
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Fin智账 API is running 🚀")
+	})
+    port := os.Getenv("PORT")
+    if port  == "" {
+        port = "8080"
+    }
+	r.Run(":" + port) // AWS Elastic Beanstalk 默认端口
+}
